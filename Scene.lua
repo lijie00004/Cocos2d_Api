da852da372f97d1e5fc95cad998ac99ae1a9f6c6
lijie00004 <lijie00004@163.com>
2019-01-10 14:43:55 +0800

    revise Scene.lua

--- Scene.lua
+++ Scene.lua
@@ -1,3 +1,45 @@
+--场景过渡动画
+    local ts = cc.TransitionJumpZoom:create(1.0, settingScene)--第一个参数是动画持续时间，第二个是场景对象
+               --TransitionFadeTR       网格过渡动画，从左下到右上
+               --TransitionJumpZoom     跳动过渡动画
+               --TransitionCrossFade    交叉渐变过渡动画
+               --TransitionMoveInL      从左边推入覆盖过渡动画
+               --TransitionShrinkGrow   放缩交替的过渡动画
+               --TransitionRotoZoom     类似照相机镜头旋转放缩交替的过渡动画
+               --TransitionSlideInL     从左侧推入的过渡动画
+               --TransitionSplitCols    按列分割界面的过渡动画
+               --TransitionSplitRows    按行分割界面的过渡动画
+               --TransitionTurnOffTiles 生产随机瓦片方格的过渡动画
+
+    cc.Director:getInstance():pushScene(ts)
+--切换场景
+    cc.Director:getInstance():runWithScene(gameScene)--只能启动第一个场景时调用
+    cc.Director:getInstance():replaceScene(gameScene)--切换到下个场景
+    cc.Director:getInstance():pushScene(settingScene)--将当前场景挂起放到场景堆栈中，然后切换到下个场景
+    cc.Director:getInstance():popScene()--与pushScene配合使用，返回上个场景
+    cc.Director:getInstance():popToRootScene()--与pushScene配合使用，返回根场景
+--场景生命周期
+    --pushScene
+        nextScene:ctor()
+        --currentScene:onExitTransitionStart()
+        nextScene:onEnter()
+        --currentScene:onExit()
+        nextScene:enterTransitionFinish()
+     --replaceScene
+        nextScene:ctor()
+        --currentScene:onExitTransitionStart()
+        nextScene:onEnter()
+        --currentScene:onExit()
+        nextScene:enterTransitionFinish()
+        --currentScene:cleanup()
+    --popScene
+        --currentScene:onExitTransitionStart()
+        --currentScene:onExit()
+        --currentScene:cleanup()
+        nextScene:onEnter()
+        nextScene:enterTransitionFinish()
+
+
 local size = cc.Director:getInstance():getWinSize()
 
 local scene = require("MyActionScene")
@@ -21,15 +63,15 @@ function LoadingScene:ctor()--相当init
     --场景生命周期事件处理
     --cc.SpriteFrameCache:getInstance():addSpriteFrames("help/help.plist")--缓存资源（长周期），一直都在的可以在main中加载
     local function onNodeEvent(event)
-        if event == "enter" then
+        if event == "enter" then--进入场景时触发
             self:onEnter()
-        elseif event == "enterTransitionFinish" then--是场景进入并且过渡动画完成时候触发
+        elseif event == "enterTransitionFinish" then--场景进入并且过渡动画完成时候触发
             self:onEnterTransitionFinish()
-        elseif event == "exit" then
+        elseif event == "exit" then--退出场景时触发
             self:onExit()
-        elseif event == "exitTransitionStart" then--是场景退出过渡动画开始时候触发
+        elseif event == "exitTransitionStart" then--场景退出过渡动画开始时候触发
             self:onExitTransitionStart()
-        elseif event == "cleanup" then--是场景销毁时候触发
+        elseif event == "cleanup" then--场景销毁时候触发
             self:cleanup()
         end
     end
