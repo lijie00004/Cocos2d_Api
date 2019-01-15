4cb868d1a14b74ebcd8e8d9ccb5cf8121f644431
lijie00004 <lijie00004@163.com>
2019-01-15 15:59:43 +0800

    auido event

--- Audio.lua
+++ Audio.lua
@@ -5,11 +5,14 @@
 --预处理放在任何场景层，进入这个层都比较“卡”，最好放到main()函数中
 AuidoEngine.preloadMusic("sound/Synth.mp3")--预加载音乐
 AuidoEngine.preloadEffect("sound/Blip.wav")--预加载音效
-
 AuidoEngine.unloadEffect("sound/Blip.wav")--清除音效缓存，没有清理背景音乐缓存函数
-
-AuidoEngine.playMusic("sound/Jazz.mp3",true)
 AudioEngine.playEffect("sound/Blip.wav")
-AuidoEngine.stopMusic
+
+AuidoEngine.playMusic("sound/Jazz.mp3",true)--true循环播放，默认false
+AuidoEngine.stopMusic()--停止播放
+AuidoEngine.pauseMusic()--暂停播放
+AuidoEngine.resumeMusic()--继续播放
+AuidoEngine.isMusicPlaying()--判断音乐是否播放
+
 
 
--- EventListenerTouch.lua
+++ EventListenerTouch.lua
@@ -1,97 +1,60 @@
+
 --单点触摸事件
-local function touchBegan(touch, event)
-    cclog("touchBegan")
-    -- 获取事件所绑定的 node
-    local node = event:getCurrentTarget()
-    -- 获取当前点击点所在相对按钮的位置坐标
-    local locationInNode = node:convertToNodeSpace(touch:getLocation())
-    local s = node:getContentSize()
-    local rect = cc.rect(0, 0, s.width, s.height)
-    -- 点击范围判断检测
-    if cc.rectContainsPoint(rect, locationInNode) then
-        cclog("sprite x = %d, y = %d ", locationInNode.x, locationInNode.y)
-        cclog("sprite tag = %d", node:getTag())
-        node:runAction(cc.ScaleBy:create(0.06, 1.06))
-        return true
+    local function touchBegan(touch, event)
+        -- 获取事件所绑定的 node
+        local node = event:getCurrentTarget()
+        -- 获取当前点击点所在相对按钮的位置坐标
+        local locationInNode = node:convertToNodeSpace(touch:getLocation())
+        local s = node:getContentSize()
+        local rect = cc.rect(0, 0, s.width, s.height)
+        -- 点击范围判断检测
+        if cc.rectContainsPoint(rect, locationInNode) then
+            cclog("sprite x = %d, y = %d ", locationInNode.x, locationInNode.y)
+            cclog("sprite tag = %d", node:getTag())
+            node:runAction(cc.ScaleBy:create(0.06, 1.06))
+            return true
+        end
+        return false
     end
 
-    return false
-end
-
-local function touchMoved(touch, event)
-    cclog("touchMoved")
-    -- 获取事件所绑定的 node
-    local node = event:getCurrentTarget()
-
-    local currentPosX, currentPosY = node:getPosition()
-    local diff = touch:getDelta()
-    -- 移动当前按钮精灵的坐标位置
-    node:setPosition(cc.p(currentPosX + diff.x, currentPosY + diff.y))
-end
-
-local function touchEnded(touch, event)
-    cclog("touchEnded")
-    -- 获取事件所绑定的 node
-    local node = event:getCurrentTarget()
-
-    local locationInNode = node:convertToNodeSpace(touch:getLocation())
-    local s = node:getContentSize()
-    local rect = cc.rect(0, 0, s.width, s.height)
-
-    -- 点击范围判断检测
-    if cc.rectContainsPoint(rect, locationInNode) then
-        cclog("sprite x = %f, y = %f ", locationInNode.x, locationInNode.y)
-        cclog("sprite tag = %d", node:getTag())
-        node:runAction(cc.ScaleTo:create(0.06, 1.0))
+    local function touchMoved(touch, event)
+        cclog("touchMoved")
+        local node = event:getCurrentTarget()
+        local currentPosX, currentPosY = node:getPosition()
+        local diff = touch:getDelta()
+        --移动当前按钮精灵的坐标位置
+        node:setPosition(cc.p(currentPosX + diff.x, currentPosY + diff.y))
     end
-end
 
---贴图的纹理图片宽高必须是2的n次幂，128x128
-local bg = cc.Sprite:create("BackgroundTile.png", cc.rect(0, 0, size.width, size.height))
---贴图的纹理参数，水平重复平铺，垂直重复平铺
-bg:getTexture():setTexParameters(gl.LINEAR, gl.LINEAR, gl.REPEAT, gl.REPEAT)
-bg:setPosition(cc.p(size.width / 2, size.height / 2))
-layer:addChild(bg, 0)
+    local function touchEnded(touch, event)
+        cclog("touchEnded")
+        local node = event:getCurrentTarget()
+        local locationInNode = node:convertToNodeSpace(touch:getLocation())
+        local s = node:getContentSize()
+        local rect = cc.rect(0, 0, s.width, s.height)
+        -- 点击范围判断检测
+        if cc.rectContainsPoint(rect, locationInNode) then
+            cclog("sprite x = %f, y = %f ", locationInNode.x, locationInNode.y)
+            cclog("sprite tag = %d", node:getTag())
+            node:runAction(cc.ScaleTo:create(0.06, 1.0))
+        end
+    end
 
-local boxA = cc.Sprite:create("BoxA2.png")
-boxA:setPosition(cc.p(size.width / 2 - 120, size.height / 2 + 120))
-layer:addChild(boxA, 10, kBoxA_Tag)
-
-local boxB = cc.Sprite:create("BoxB2.png")
-boxB:setPosition(cc.p(size.width / 2, size.height / 2))
-layer:addChild(boxB, 20, kBoxB_Tag)
-
-local boxC = cc.Sprite:create("BoxC2.png")
-boxC:setPosition(cc.p(size.width / 2 + 120, size.height / 2 + 160))
-layer:addChild(boxC, 30, kBoxC_Tag)
-
-
-
-local function touchMoved(touch, event)
-    -- 获取事件所绑定的 node
-    local node = event:getCurrentTarget()
-    local currentPosX, currentPosY = node:getPosition()
-    local diff = touch:getDelta()
-    -- 移动当前按钮精灵的坐标位置
-    node:setPos(cc.p(currentPosX + diff.x, currentPosY + diff.y))
-end
 -- 创建一个事件监听器 OneByOne 为单点触摸
-local listener1 = cc.EventListenerTouchOneByOne:create()
--- 设置是否吞没事件，在 onTouchBegan 方法返回 true 时吞没
-listener1:setSwallowTouches(true)
--- EVENT_TOUCH_BEGAN事件回调函数
-listener1:registerScriptHandler(touchBegan, cc.Handler.EVENT_TOUCH_BEGAN)
--- EVENT_TOUCH_MOVED事件回调函数
-listener1:registerScriptHandler(touchMoved, cc.Handler.EVENT_TOUCH_MOVED)
--- EVENT_TOUCH_ENDED事件回调函数
-listener1:registerScriptHandler(touchEnded, cc.Handler.EVENT_TOUCH_ENDED)
+    local listener1 = cc.EventListenerTouchOneByOne:create()
+    -- 设置是否吞没事件，在 onTouchBegan 方法返回 true 时吞没
+    listener1:setSwallowTouches(true)
+    -- EVENT_TOUCH_BEGAN事件回调函数
+    listener1:registerScriptHandler(touchBegan, cc.Handler.EVENT_TOUCH_BEGAN)--EVENT_TOUCH_MOVED--EVENT_TOUCH_ENDED
 
-local eventDispatcher = cc.Director:getInstance():getEventDispatcher()
--- 添加监听器
-eventDispatcher:addEventListenerWithSceneGraphPriority(listener1, boxA)
-local listener2 = listener1:clone()
-eventDispatcher:addEventListenerWithSceneGraphPriority(listener2, boxB)
-local listener3 = listener1:clone()
-eventDispatcher:addEventListenerWithSceneGraphPriority(listener3, boxC)
+    local eventDispatcher = cc.Director:getInstance():getEventDispatcher()
+    -- 添加监听器
+    --设置触摸绑定node，优先级为默认的0，当节点销毁时，同时销毁添加的事件监听
+    eventDispatcher:addEventListenerWithSceneGraphPriority(listener1, boxA)--node来决定Listenter的优先级，node越靠前越优先
+    local listener2 = listener1:clone()
+    --指定Listenter的优先级，越小越优先,不绑定节点
+    eventDispatcher:addEventListenerWithFixedPriority(listener2, 1)
 
-eventDispatcher:removeEventListener(listener1)
+    eventDispatcher:removeEventListener(listener1)--注销指定事件监听器
+    eventDispatcher:removeCustomEventListeners(customEventName)--注销自定义事件监听器
+    eventDispatcher:removeAllEventListeners()--注销所有事件监听器，包括菜单项
